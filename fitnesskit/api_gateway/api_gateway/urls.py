b33ba5d3d92f7669c3b58c7121f923d65636956e
kaminyv <kaminyv@yandex.ru>
2023-01-12 13:47:47 +0600

    Тестовое задание FitnessKit

--- fitnesskit/api_gateway/api_gateway/urls.py
+++ fitnesskit/api_gateway/api_gateway/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('team/', include('team_one_c.urls'))
+    path('team/', include('team_one_c.urls')),
+    path('', admin.site.urls),
 ]
